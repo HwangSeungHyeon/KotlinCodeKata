847bd616dc0c64c6ceb3301b5316d29c44d3677b
HwangSeungHyeon <xlddy02@gmail.com>
2023-12-04 17:06:44 +0900

    level 1 update

---
+++ level1/1_두 수의 차.kt
@@ -0,0 +1,8 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120803
+// 정수 num1과 num2가 주어질 때, num1에서 num2를 뺀 값을 return하도록 soltuion 함수를 완성해주세요.
+
+class Solution {
+    fun solution(num1: Int, num2: Int): Int {
+        return num1-num2
+    }
+}
---
+++ level1/3_몫 구하기.kt
@@ -0,0 +1,10 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120805
+// 정수 num1, num2가 매개변수로 주어질 때, num1을 num2로 나눈 몫을 return 하도록 solution 함수를 완성해주세요.
+
+class Solution {
+    fun solution(num1: Int, num2: Int): Int {
+        var answer: Int = 0
+        answer = num1/num2
+        return answer
+    }
+}

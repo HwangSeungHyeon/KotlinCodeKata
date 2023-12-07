99bad39b2fd2cbecf5f4dafceb9cae87bd9fadf2
HwangSeungHyeon <xlddy02@gmail.com>
2023-12-07 09:54:03 +0900

    add solves

---
+++ level1/030_가운데 글자 가져오기.kt
@@ -0,0 +1,20 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/12903
+// 단어 s의 가운데 글자를 반환하는 함수, solution을 만들어 보세요. 단어의 길이가 짝수라면 가운데 두글자를 반환하면 됩니다.
+
+class Solution {
+    fun solution(s: String): String {
+        var answer = ""
+        var strLen = s.length
+        var strLenHalf = strLen/2
+        
+        if(strLen%2==0){ // 단어 길이가 짝수일 경우
+            println("문자열 길이: ${strLen}")
+            answer = s[strLenHalf-1].toString() + s[strLenHalf]
+        }
+        else{ //단어 길이가 홀수일 경우
+            answer = s[strLenHalf].toString()
+        }
+        
+        return answer
+    }
+}

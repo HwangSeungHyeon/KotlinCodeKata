7ce8d0cb55ce820943a0faaae2bc74d48ce9f9eb
HwangSeungHyeon <xlddy02@gmail.com>
2023-12-15 18:28:47 +0900

    Edit solves

--- level1/016_x만큼 간격이 있는 n개의 숫자.kt
+++ level1/016_x만큼 간격이 있는 n개의 숫자.kt
@@ -1,14 +1,6 @@
 //https://school.programmers.co.kr/learn/courses/30/lessons/12954
 //함수 solution은 정수 x와 자연수 n을 입력 받아, x부터 시작해 x씩 증가하는 숫자를 n개 지니는 리스트를 리턴해야 합니다. 다음 제한 조건을 보고, 조건을 만족하는 함수, solution을 완성해주세요.
 
-
 class Solution {
-    fun solution(x: Int, n: Int): LongArray {
-        var answer = mutableListOf<Long>() //먼저 수정 가능한 Long 타입 리스트 선언
-
-        for(i in 1..n){
-            answer.add(x.toLong()*i) //Long 타입 배열에 넣기 위해 x를 Long으로 변경
-        }
-        return answer.toLongArray() //수정가능한 리스트에서 수정 불가능한 배열로 변경한다.
-    }
+    fun solution(x: Int, n: Int) = LongArray(n, {(it+1)*x.toLong()})
 }
--- level2/031_수박수박수박수박수박수.kt
+++ level2/031_수박수박수박수박수박수.kt
@@ -2,16 +2,9 @@
 // 길이가 n이고, "수박수박수박수...."와 같은 패턴을 유지하는 문자열을 리턴하는 함수, solution을 완성하세요. 예를들어 n이 4이면 "수박수박"을 리턴하고 3이라면 "수박수"를 리턴하면 됩니다.
 
 class Solution {
-    fun solution(n: Int): String {
-        var answer = ""
-        for(i in 0 until n){
-            if(i%2 == 0) { // 짝수일 경우
-                answer += "수"
-            }
-            else{ // 홀수일 경우
-                answer += "박"
-            }
-        }
-        return answer
-    }
+    // String() : CharArray를 문자열로 바꾸기 위해서 사용
+    // CharArray() : 문자를 저장할 배열
+    // n : 배열의 크기, 0부터 n-1까지 반복한다는 뜻
+    // {it -> if(it%2==0)'수' else '박'} : 0부터 n-1 사이의 어떤 수가 짝수면 '수' 홀수면 '박'을 배열에 저장하겠다는 뜻
+    fun solution(n: Int) = String(CharArray(n, { it -> if(it%2==0)'수' else '박'}))
 }

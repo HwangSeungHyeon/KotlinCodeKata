873a3f99eb3801c3013b56d551c3e201405bbf49
HwangSeungHyeon <xlddy02@gmail.com>
2023-12-22 21:55:39 +0900

    EDIT: "Edit duplicate code"
    중복되는 부분 제거

--- level1/030_가운데 글자 가져오기.kt
+++ level1/030_가운데 글자 가져오기.kt
@@ -2,18 +2,10 @@
 // 단어 s의 가운데 글자를 반환하는 함수, solution을 만들어 보세요. 단어의 길이가 짝수라면 가운데 두글자를 반환하면 됩니다.
 
 class Solution {
-    fun solution(s: String): String {
-        var answer = ""
-        var strLen = s.length
-        var strLenHalf = strLen/2
-        
-        if(strLen%2==0){ // 단어 길이가 짝수일 경우
-            answer += s[strLenHalf-1].toString() + s[strLenHalf]
-        }
-        else{ //단어 길이가 홀수일 경우
-            answer += s[strLenHalf].toString()
-        }
-        
+    fun solution(s: String): String{
+        var half = s.length/2
+        var answer = s[half].toString()
+        if(s.length %2 == 0) answer = s[half-1] + answer
         return answer
     }
 }

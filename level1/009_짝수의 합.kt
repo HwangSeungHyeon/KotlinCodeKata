02e76108721fd81bf577e55d57027a0dc5c65f49
HwangSeungHyeon <xlddy02@gmail.com>
2023-12-22 19:51:56 +0900

    EDIT: "Edit code's time complexity O(N) to O(1)"
    시간 복잡도를 O(N)에서 O(1)로 수정

--- level1/009_짝수의 합.kt
+++ level1/009_짝수의 합.kt
@@ -2,8 +2,5 @@
 // 정수 n이 주어질 때, n이하의 짝수를 모두 더한 값을 return 하도록 solution 함수를 작성해주세요.
 
 class Solution {
-    //(0..n) => 0에서 n까지 범위 지정
-    //filter{it%2==0} => 0에서 n까지 숫자 중 짝수만 가져옴
-    //sum() => 가져온 짝수를 모두 더함
-    fun solution(n: Int) = (0..n).filter{it % 2 == 0}.sum()
+    fun solution(n: Int) = n/2*(n/2+1)
 }

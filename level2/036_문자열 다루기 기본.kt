30665cab02864ee0b6c062fca7bbfdfd1ac60733
HwangSeungHyeon <xlddy02@gmail.com>
2024-01-05 17:34:35 +0900

    EDIT: "edit the answer code for question 36"
    문자열 다루기 기본 문제 풀이 수정

--- level2/036_문자열 다루기 기본.kt
+++ level2/036_문자열 다루기 기본.kt
@@ -2,13 +2,5 @@
 //문자열 s의 길이가 4 혹은 6이고, 숫자로만 구성돼있는지 확인해주는 함수, solution을 완성하세요. 예를 들어 s가 "a234"이면 False를 리턴하고 "1234"라면 True를 리턴하면 됩니다.
 
 class Solution {
-    fun solution(s: String) = if(s.length != 4 && s.length != 6) false else {
-        try{
-            if(s.toInt() is Int) true
-            else false
-        }catch(e:Exception){
-            false
-        }
-    }
-
+    fun solution(s: String) = (s.length == 4 || s.length == 6) && (s.toIntOrNull() != null)
 }
